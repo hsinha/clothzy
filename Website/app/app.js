134e7a191863c3389e31b08ad24587ff6cb5be13
gpranav88 <gpranav77@gmail.com>
2013-10-21 00:02:44 -0700

    angular support for product page

---
+++ Website/app/app.js
@@ -0,0 +1,57 @@
+﻿/*#######################################################################
+  
+  Pranav Gupta
+ Current folder structure:
+
+  /app
+      /controllers      
+      /directives
+      /services
+      /partials
+      /views
+
+  #######################################################################*/
+
+var app = angular.module('masterApp', []);
+
+//This configures the routes and associates each route with a view and a controller
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/stores',
+            {
+                controller: 'StoresController',
+                templateUrl: '/app/partials/storeID.html'
+            })
+        //Define a route that has a route parameter in it (:customerID)
+        .when('/stores/:storeID',
+            {
+                controller: 'StoreController',
+                templateUrl: '/app/partials/storeID.html'
+            })
+        //Define a route that has a route parameter in it (:customerID)
+        .when('/products',
+            {
+                controller: 'ProductsController',
+                templateUrl: '/app/partials/products.html'
+            })
+        .when('/products/:productID',
+            {
+                controller: 'ProductController',
+                templateUrl: '/app/partials/productID.html'
+            })
+        .when('/reviewers',
+            {
+                controller: 'ReviewersController',
+                templateUrl: '/app/partials/reviewers.html'
+            })
+        .when('/reviewers/:reviewerID',
+            {
+                controller: 'ReviewerController',
+                templateUrl: '/app/partials/reviewerID.html'
+            })
+        .otherwise({ redirectTo: '/stores' });
+});
+
+
+
+
---
+++ Website/app/services/productsService.js
@@ -0,0 +1,67 @@
+﻿//This handles retrieving data and is used by controllers. 3 options (server, factory, provider) with 
+//each doing the same thing just structuring the functions/data differently.
+app.service('productsService', function () {
+    this.getProducts = function () {
+        return Products;
+    };
+
+    this.insertProduct = function (firstName, lastName, city) {
+        var topID = Products.length + 1;
+        Products.push({
+            id: topID,
+            firstName: firstName,
+            lastName: lastName,
+            city: city
+        });
+    };
+
+    this.deleteProduct = function (id) {
+        for (var i = Products.length - 1; i >= 0; i--) {
+            if (Products[i].id === id) {
+                Products.splice(i, 1);
+                break;
+            }
+        }
+    };
+
+    this.getProduct = function (id) {
+        for (var i = 0; i < Products.length; i++) {
+            if (Products[i].id === id) {
+                return Products[i];
+            }
+        }
+        return null;
+    };
+
+    var Products = [
+        {
+         "id": "1",
+         "name": "Levis Blue Denim",
+         "imgPath": "img/prods/p1.jpg",
+         "store":  "Store XYZ, Hauz Khas Village",
+         "price": "1500"
+      },
+      {
+         "id": "2",
+         "name": "Pepe Black Jeans",
+         "imgPath": "img/prods/p2.jpg",
+         "store":  "Store ABC, Hauz Khas Village",
+         "price": "1200"
+      },
+      {
+         "id": "3",
+         "name": "Pepe Blue Jeans",
+         "imgPath": "img/prods/p3.jpg",
+         "store":  "Store DEF, Hauz Khas Village",
+         "price": "3500"
+      },
+      {
+         "id": "3",
+         "name": "Pepe Blue Jeans",
+         "imgPath": "img/prods/p4.jpg",
+         "store":  "Store DEF, Hauz Khas Village",
+         "price": "3500"
+      }
+    ];
+
+});
