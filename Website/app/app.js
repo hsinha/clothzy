08f76183fff82eeb3cdf4737ec02d515a51afca8
Harsh <mail@hsinha.me>
2013-11-05 12:07:15 +0530

    Test

--- Website/app/app.js
+++ Website/app/app.js
@@ -12,46 +12,59 @@
 
   #######################################################################*/
 
-var app = angular.module('masterApp', []);
+//var app = angular.module('masterApp', []);
 
 //This configures the routes and associates each route with a view and a controller
-app.config(function ($routeProvider) {
-    $routeProvider
-        .when('/stores',
-            {
-                controller: 'StoresController',
-                templateUrl: '/app/partials/storeID.html'
-            })
-        //Define a route that has a route parameter in it (:customerID)
-        .when('/stores/:storeID',
-            {
-                controller: 'StoreController',
-                templateUrl: '/app/partials/storeID.html'
-            })
-        //Define a route that has a route parameter in it (:customerID)
-        .when('/products',
-            {
-                controller: 'ProductsController',
-                templateUrl: '/app/partials/products.html'
-            })
-        .when('/products/:productID',
-            {
-                controller: 'ProductController',
-                templateUrl: '/app/partials/productID.html'
-            })
-        .when('/reviewers',
-            {
-                controller: 'ReviewersController',
-                templateUrl: '/app/partials/reviewers.html'
-            })
-        .when('/reviewers/:reviewerID',
-            {
-                controller: 'ReviewerController',
-                templateUrl: '/app/partials/reviewerID.html'
-            })
-        .otherwise({ redirectTo: '/stores' });
-});
+//app.config(function ($routeProvider) {
+//    $routeProvider
+//        .when('/stores',
+//            {
+//                controller: 'StoresController',
+//                templateUrl: '/app/partials/storeID.html'
+//            })
+//        //Define a route that has a route parameter in it (:customerID)
+//        .when('/stores/:storeID',
+//            {
+//                controller: 'StoreController',
+//                templateUrl: '/app/partials/storeID.html'
+//            })
+//        //Define a route that has a route parameter in it (:customerID)
+//        .when('/products',
+//            {
+//                controller: 'ProductsController',
+//                templateUrl: '/app/partials/products.html'
+//            })
+//        .when('/products/:productID',
+//            {
+//                controller: 'ProductController',
+//                templateUrl: '/app/partials/productID.html'
+//            })
+//        .when('/reviewers',
+//            {
+//                controller: 'ReviewersController',
+//                templateUrl: '/app/partials/reviewers.html'
+//            })
+//        .when('/reviewers/:reviewerID',
+//            {
+//                controller: 'ReviewerController',
+//                templateUrl: '/app/partials/reviewerID.html'
+//            })
+//        .otherwise({ redirectTo: '/stores' });
+//});
+//
+//
+//
+//
+//
 
-
-
-
+angular
+    .module('guest',[])
+    .config(function($routeProvider){
+        $routeProvider
+            .when ('/',{
+                templateUrl:'app/views/homePage.html'
+            });
+    })
+    .controller('homePage',function($scope){
+        //dummy json
+    });
